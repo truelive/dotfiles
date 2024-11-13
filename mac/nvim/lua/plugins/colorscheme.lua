61590293b65043845af6ff032c00936c9c9d1eb2
Roman Schweitzer <roman.schweitzer@gmail.com>
2024-11-13 12:19:43 +0100

    lsp change and full setup

--- mac/nvim/lua/plugins/colorscheme.lua
+++ mac/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,18 @@
 return {
+  {
     "tiagovla/tokyodark.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme tokyodark")
+        -- vim.cmd("colorscheme tokyodark")
     end,
+  },
+  {
+    "rmehri01/onenord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function ()
+      vim.cmd("colorscheme onenord")
+    end,
+  },
 }
